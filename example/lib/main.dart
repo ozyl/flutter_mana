354a4fa308dc0caff888698ec5f15b23edcdcbe9
lhlyu <misakafs@gmail.com>
2025-08-14 10:36:02 +0800

    fix: soft keyboard occlusion

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -105,6 +105,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                 backgroundColor: Colors.deepPurple,
                 onPressed: addSharedPreferences,
               ),
+              TextField(),
             ],
           ),
         ),
